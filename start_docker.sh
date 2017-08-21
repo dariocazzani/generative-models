01062b336c5ce85d26e3deab81e8cfc7a1af9909
Dario Cazzani <dcazzani@cisco.com>
2017-08-21 16:23:51 -0700

    Implemented start docker script

--- start_docker.sh
+++ start_docker.sh
@@ -1,6 +1,14 @@
+export MAIN_PATH="/share/generative-models"
 port=${1:-8880}
 
-nvidia-docker run --privileged --rm -it \
+xhost +
+GPU=0 nvidia-docker run --privileged --rm -it \
+  --env DISPLAY=$DISPLAY \
+  --env="QT_X11_NO_MITSHM=1" \
+  -e MAIN_PATH=$MAIN_PATH \
+  -v /dev/video0:/dev/video0 \
+  -v /dev/video1:/dev/video1 \
+  -v /tmp/.X11-unix:/tmp/.X11-unix:ro  \
   -v $PWD:/root/generative-models \
   -p $port:$port \
   -v /share:/share \
