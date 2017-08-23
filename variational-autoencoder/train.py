9370066ef29dbf8727327d1d6abb9a7d7cde2cd4
Dario Cazzani <dcazzani@cisco.com>
2017-08-23 14:42:14 -0700

    Added TensorBoard logging

--- variational-autoencoder/train.py
+++ variational-autoencoder/train.py
@@ -23,7 +23,7 @@ from tensorflow.examples.tutorials.mnist import input_data
 mnist = input_data.read_data_sets('../../MNIST_data', one_hot=True)
 
 # Get the MNIST data
-mnist = input_data.read_data_sets('./Data', one_hot=True)
+mnist = input_data.read_data_sets('../Data', one_hot=True)
 
 # Parameters
 input_dim = mnist.train.images.shape[1]
@@ -91,20 +91,20 @@ def decoder(z, reuse=False):
     prob = tf.nn.sigmoid(logits)
     return prob, logits
 
-
-# =============================== TRAINING ====================================
-
 def train(options):
     with tf.name_scope('Input'):
         X = tf.placeholder(tf.float32, shape=[None, input_dim])
+        input_images = tf.reshape(X, [-1, 28, 28, 1])
 
     with tf.name_scope('Latent_variable'):
         z = tf.placeholder(tf.float32, shape=[None, options.z_dim])
 
-    with tf.variable_scope(tf.get_variable_scope()):
-        z_mu, z_logvar = encoder(X)
-        z_sample = sample_z(z_mu, z_logvar)
-        _, logits = decoder(z_sample)
+    with tf.name_scope('Autoencoder'):
+        with tf.variable_scope(tf.get_variable_scope()):
+            z_mu, z_logvar = encoder(X)
+            z_sample = sample_z(z_mu, z_logvar)
+            decoder_output, logits = decoder(z_sample)
+            generated_images = tf.reshape(decoder_output, [-1, 28, 28, 1])
 
     with tf.variable_scope(tf.get_variable_scope()):
         # Sampling from random z
@@ -112,39 +112,70 @@ def train(options):
 
 
     # E[log P(X|z)]
-    recon_loss = tf.reduce_sum(tf.nn.sigmoid_cross_entropy_with_logits(logits=logits, labels=X), 1)
+    reconstruction_loss = tf.reduce_sum(tf.nn.sigmoid_cross_entropy_with_logits(logits=logits, labels=X), 1)
     # D_KL(Q(z|X) || P(z|X)); calculate in closed form as both dist. are Gaussian
     kl_loss = 0.5 * tf.reduce_sum(tf.exp(z_logvar) + z_mu**2 - 1. - z_logvar, 1)
     # VAE loss
-    vae_loss = tf.reduce_mean(recon_loss + kl_loss)
+    vae_loss = tf.reduce_mean(reconstruction_loss + kl_loss)
 
     # Optimizer
     train_op, grads_and_vars = AdamOptimizer(vae_loss, options.learning_rate, options.beta1)
 
-    sess = tf.Session()
-    sess.run(tf.global_variables_initializer())
+    # Visualization
+    tf.summary.scalar(name='Loss', tensor=vae_loss)
+    tf.summary.histogram(name='Encoder z_mu', values=z_mu)
+    tf.summary.histogram(name='Encoder z_logvar', values=z_logvar)
+    tf.summary.histogram(name='Sampled variable', values=z_sample)
 
-    if not os.path.exists('out/'):
-        os.makedirs('out/')
+    for grad, var in grads_and_vars:
+        tf.summary.histogram(var.name + '/gradient', grad)
 
-    i = 0
+    tf.summary.image(name='Input Images', tensor=input_images, max_outputs=10)
+    tf.summary.image(name='Generated Images', tensor=generated_images, max_outputs=10)
+    summary_op = tf.summary.merge_all()
 
-    for it in range(1000000):
-        X_mb, _ = mnist.train.next_batch(options.batch_size)
 
-        _, loss = sess.run([train_op, vae_loss], feed_dict={X: X_mb})
+    saver = tf.train.Saver()
+    step = 0
+    init = tf.global_variables_initializer()
+    with tf.Session() as sess:
+        sess.run(init)
+        try:
+            writer = tf.summary.FileWriter(logdir=options.tensorboard_path, graph=sess.graph)
+            if not os.path.exists('out/'):
+                os.makedirs('out/')
+            for i in range(options.epochs):
+                n_batches = int(mnist.train.num_examples / options.batch_size)
+                for b in range(n_batches):
+                    batch_x, _ = mnist.train.next_batch(options.batch_size)
 
-        if it % 1000 == 0:
-            print('Iter: {}'.format(it))
-            print('Loss: {:.4}'. format(loss))
-            print()
+                    # Train
+                    sess.run(train_op, feed_dict={X: batch_x})
 
-            samples = sess.run(X_samples, feed_dict={z: np.random.randn(16, options.z_dim)})
+                    if b % 50 == 0:
+                        summary, batch_loss = sess.run([summary_op, vae_loss], feed_dict={X: batch_x})
+                        writer.add_summary(summary, global_step=step)
+                        print("Loss: {}".format(batch_loss))
+                        print("Epoch: {}, iteration: {}".format(i, b))
+                        print()
 
-            fig = plot(samples)
-            plt.savefig('out/{}.png'.format(str(i).zfill(3)), bbox_inches='tight')
-            i += 1
-            plt.close(fig)
+                        samples = sess.run(X_samples, feed_dict={z: np.random.randn(16, options.z_dim)})
+
+                        fig = plot(samples)
+                        plt.savefig('out/{}.png'.format(str(i).zfill(3)), bbox_inches='tight')
+                        plt.close(fig)
+                    step += 1
+                saver.save(sess, save_path=options.checkpoints_path, global_step=step)
+            print("Model Trained!")
+            print("Tensorboard Path: {}".format(options.tensorboard_path))
+            print("Log Path: {}".format(options.logs_path + '/log.txt'))
+            print("Saved Model Path: {}".format(options.checkpoints_path))
+
+        except KeyboardInterrupt:
+            print('Stopping training...')
+            print("Saved Model Path: {}".format(options.checkpoints_path))
+            saver.save(sess, save_path=options.checkpoints_path, global_step=step)
+
 
 if __name__ == '__main__':
     check_tf_version()
@@ -154,5 +185,5 @@ if __name__ == '__main__':
         dir_path = os.path.dirname(os.path.realpath(__file__))
         cur_dir = dir_path.split('/')[-1]
         options = extend_options(parser, cur_dir)
-
+    x_input = tf.placeholder(dtype=tf.float32, shape=[options.batch_size, input_dim], name='Input')
     train(options)
