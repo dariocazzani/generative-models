f7b9fcd7bf0655a5473f720f9509505f96e8142f
Dario Cazzani <dcazzani@cisco.com>
2017-09-13 11:26:39 -0700

    Implemented conditional variational autoencoder

---
+++ conditional-variational-autoencoder/train_mnist_FC.py
@@ -0,0 +1,162 @@
+#!/usr/bin/python
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+"""
+Authors:    Dario Cazzani
+"""
+import sys
+sys.path.append('../')
+from config import set_config
+from helpers.misc import check_tf_version, extend_options
+from helpers.graph import get_variables, linear, AdamOptimizer
+from helpers.display import plot
+import subprocess
+import tensorflow as tf
+import numpy as np
+import glob
+import os
+from tensorflow.examples.tutorials.mnist import input_data
+
+# Get the MNIST data
+mnist = input_data.read_data_sets('../Data', one_hot=True)
+
+# Parameters
+input_dim = mnist.train.images.shape[1]
+label_dim = mnist.train.labels.shape[1]
+print(label_dim)
+hidden_layer1 = 1000
+hidden_layer2 = 1000
+
+# Q(z|X)
+def encoder(X, c, reuse=False):
+    if reuse:
+        tf.get_variable_scope().reuse_variables()
+
+    with tf.name_scope('Condition_input'):
+        inputs = tf.concat(axis=1, values=[X, c])
+
+    e_linear_1 = tf.nn.relu(linear(inputs, hidden_layer1, 'e_linear_1'))
+    e_linear_2 = tf.nn.relu(linear(e_linear_1, hidden_layer2, 'e_linear_2'))
+    z_mu = linear(e_linear_2, options.z_dim, 'z_mu')
+    z_logvar = linear(e_linear_2, options.z_dim, 'z_logvar')
+    return z_mu, z_logvar
+
+def sample_z(mu, log_var):
+    eps = tf.random_normal(shape=tf.shape(mu))
+    return mu + tf.exp(log_var / 2) * eps
+
+# P(X|z)
+def decoder(z, c, reuse=False):
+    if reuse:
+        tf.get_variable_scope().reuse_variables()
+
+    with tf.name_scope('Condition_latent_variable'):
+        inputs = tf.concat(axis=1, values=[z, c])
+
+    d_linear_1 = tf.nn.relu(linear(inputs, hidden_layer2, 'd_linear_1'))
+    d_linear_2 = tf.nn.relu(linear(d_linear_1, hidden_layer1, 'd_linear_2'))
+    logits = linear(d_linear_2, input_dim, 'logits')
+    prob = tf.nn.sigmoid(logits)
+    return prob, logits
+
+def train(options):
+    with tf.name_scope('Input'):
+        X = tf.placeholder(tf.float32, shape=[None, input_dim])
+        input_images = tf.reshape(X, [-1, 28, 28, 1])
+
+    with tf.name_scope('Condition'):
+        c = tf.placeholder(tf.float32, shape=[None, label_dim])
+
+    with tf.name_scope('Latent_variable'):
+        z = tf.placeholder(tf.float32, shape=[None, options.z_dim])
+
+    with tf.name_scope('Autoencoder'):
+        with tf.variable_scope(tf.get_variable_scope()):
+            z_mu, z_logvar = encoder(X, c)
+            z_sample = sample_z(z_mu, z_logvar)
+            decoder_output, logits = decoder(z_sample, c)
+            generated_images = tf.reshape(decoder_output, [-1, 28, 28, 1])
+
+    with tf.variable_scope(tf.get_variable_scope()):
+        # Sampling from random z
+        X_samples, _ = decoder(z, c, reuse=True)
+
+    with tf.name_scope('Loss'):
+        # E[log P(X|z)]
+        reconstruction_loss = tf.reduce_sum(tf.nn.sigmoid_cross_entropy_with_logits(logits=logits, labels=X), 1)
+        # D_KL(Q(z|X) || P(z|X)); calculate in closed form as both dist. are Gaussian
+        kl_loss = 0.5 * tf.reduce_sum(tf.exp(z_logvar) + z_mu**2 - 1. - z_logvar, 1)
+        # VAE loss
+        vae_loss = tf.reduce_mean(reconstruction_loss + kl_loss)
+
+    # Optimizer
+    train_op, grads_and_vars = AdamOptimizer(vae_loss, options.learning_rate, options.beta1)
+
+    # Visualization
+    tf.summary.scalar(name='Loss', tensor=vae_loss)
+    tf.summary.histogram(name='Sampled variable', values=z_sample)
+
+    for grad, var in grads_and_vars:
+        tf.summary.histogram('Gradients/' + var.name, grad)
+        tf.summary.histogram('Values/' + var.name, var)
+
+    tf.summary.image(name='Input Images', tensor=input_images, max_outputs=10)
+    tf.summary.image(name='Generated Images', tensor=generated_images, max_outputs=10)
+    summary_op = tf.summary.merge_all()
+
+    saver = tf.train.Saver()
+    step = 0
+    init = tf.global_variables_initializer()
+    n_batches = int(mnist.train.num_examples / options.batch_size)
+    with tf.Session() as sess:
+        sess.run(init)
+        if not options.run_inference:
+            try:
+                writer = tf.summary.FileWriter(logdir=options.tensorboard_path, graph=sess.graph)
+                for epoch in range(options.epochs):
+                    for iteration in range(n_batches):
+                        batch_x, batch_y = mnist.train.next_batch(options.batch_size)
+
+                        # Train
+                        sess.run(train_op, feed_dict={X: batch_x, c: batch_y})
+
+                        if iteration % 50 == 0:
+                            summary, batch_loss = sess.run([summary_op, vae_loss], feed_dict={X: batch_x, c: batch_y})
+                            writer.add_summary(summary, global_step=step)
+                            print("Epoch: {} - Iteration {} - Loss: {:.4f}\n".format(epoch, iteration, batch_loss))
+
+                        step += 1
+                    saver.save(sess, save_path=options.checkpoints_path, global_step=step)
+                print("Model Trained!")
+
+            except KeyboardInterrupt:
+                print('Stopping training...')
+                saver.save(sess, save_path=options.checkpoints_path, global_step=step)
+        else:
+            print('Restoring latest saved TensorFlow model...')
+            dir_path = os.path.dirname(os.path.realpath(__file__))
+            cur_dir = dir_path.split('/')[-1]
+            script_name = os.path.basename(__file__).split('.')[0]
+            experiments = glob.glob(os.path.join(options.MAIN_PATH, cur_dir) + '/{}*'.format(script_name))
+            experiments.sort(key=lambda x: os.path.getmtime(x))
+            if len(experiments) > 0:
+                print('Restoring: {}'.format(experiments[-1]))
+                saver.restore(sess, tf.train.latest_checkpoint(os.path.join(experiments[-1], 'checkpoints')))
+                plot(sess, z, X_samples, num_images=15, height=28, width=28, condition=True, c=c)
+
+            else:
+                print('No checkpoint found at {}'.format(os.path.join(options.MAIN_PATH, cur_dir)))
+
+if __name__ == '__main__':
+    check_tf_version()
+    parser = set_config()
+    (options, args) = parser.parse_args()
+    if not options.run_inference:
+        dir_path = os.path.dirname(os.path.realpath(__file__))
+        cur_dir = dir_path.split('/')[-1]
+        script_name = os.path.basename(__file__).split('.')[0]
+        options = extend_options(parser, cur_dir, script_name)
+
+    train(options)
