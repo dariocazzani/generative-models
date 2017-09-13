1fc6294c5f5dfebdcaa0f4a5d5572026767978e8
Dario Cazzani <dcazzani@cisco.com>
2017-09-13 11:26:53 -0700

    Allow condition

--- helpers/display.py
+++ helpers/display.py
@@ -2,14 +2,31 @@ import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib import gridspec
 
-def plot(sess, z, X_samples, num_images, height, width):
+def plot(sess, z, X_samples, num_images, height, width, condition=False, c=None):
     samples = []
+    num_classes = 10
+    if condition:
+        y = np.zeros(shape=[1, num_classes])
+        digit = input('What digit do you want to generate? [0-9]: ')
+        digit2 = input('What digit do you want to combine the previous one? [0-9]: ')
+
+        digit = int(digit)
+        digit2 = int(digit2)
+        try:
+            y[:, digit] = 1.
+            y[:, digit2] = 1.
+        except Exception as e:
+            print('Could not generate condition: {}'.format(e))
+
     grid_x = np.linspace(-2, 2, num_images)
     grid_y = np.linspace(-2, 2, num_images)
     for i, yi in enumerate(grid_x):
         for j, xi in enumerate(grid_y):
             z_sample = np.array([[xi, yi]])
-            samples.append(sess.run(X_samples, feed_dict={z: z_sample}))
+            if condition:
+                samples.append(sess.run(X_samples, feed_dict={z: z_sample, c: y}))
+            else:
+                samples.append(sess.run(X_samples, feed_dict={z: z_sample}))
 
     fig = plt.figure(figsize=(12, 12))
     gs = gridspec.GridSpec(num_images, num_images)
